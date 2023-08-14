c6fc97506c558ace0b218f379a5cf32bf1386dc7
conrado julio <conradojuliosisnero@gmail.com>
2023-08-14 16:52:10 -0500

    creando componentes sencillos

---
+++ practice/src/components/TodoItems.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function TodoItems() {
+  return (
+    <div>TodoItems</div>
+  )
+}
---
+++ practice/src/components/app.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function App (){
+    return <div>Estas mamandote cule cachera</div>;
+}
