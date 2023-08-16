7f3a66c120ffa19dbc1921a60a14c847a5d5211a
conrado julio <conradojuliosisnero@gmail.com>
2023-08-16 13:53:27 -0500

    estilizando de mejor manera el form

--- practice/src/components/form/form.jsx
+++ practice/src/components/form/form.jsx
@@ -4,17 +4,25 @@ import SelectInput from "./input/selectInput";
 import Button from "./button/button";
 
 function Form({ placeholder, labelName }) {
+	const evento = (e) => {
+		e.preventDefault();
+	};
+
 	return (
-		<form className="form__contend">
-			<div className="form__tittle"><h2>Form Tittle</h2></div>
-			<div className="inputs__contend">
-			<Input labelName="Name" placeholder="Name" />
-			<Input labelName="User Name" placeholder="User Name" />
-			<Input labelName="Phone Number" placeholder="Phone Number" />
-			<SelectInput/>
-			</div>
-			<Button/>
-		</form>
+		<section className="section__form">
+			<form className="form__contend" onSubmit={evento}>
+				<div className="form__tittle">
+					<h2>Form Tittle</h2>
+				</div>
+				<div className="inputs__contend">
+					<Input labelName="Name" placeholder="Name" />
+					<Input labelName="User Name" placeholder="User Name" />
+					<Input labelName="Phone Number" placeholder="Phone Number" />
+					<SelectInput />
+				</div>
+				<Button />
+			</form>
+		</section>
 	);
 }
 
--- practice/src/components/form/input/selectInput.jsx
+++ practice/src/components/form/input/selectInput.jsx
@@ -15,14 +15,12 @@ function SelectInput({ options }) {
 	});
 
 	return (
-		<div className="select___container">
+		<div className="input__contend">
 			<label htmlFor="" className="label__input">
 				Select your Ocupation
 			</label>
 			<select name="" id="" className="select__input">
-				{equipos.map((equipo, index) => {
-					return <option>{equipo}</option>;
-				})}
+				{equipos.map( (equipo, index) => <option>{equipo}</option>)}
 			</select>
 		</div>
 	);
