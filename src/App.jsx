072394b3db60601b30000be210abcabf1c621197
conrado julio <conradojuliosisnero@gmail.com>
2023-08-19 11:17:00 -0500

    remota del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/head/header';
 import Form from './components/form/form';
+import Section_Card from './components/section/section_card';
 
 function App() {
   return (
     <React.StrictMode>
         <Header/>
         <Form/>
+        <Section_Card/>
     </React.StrictMode>
   )
 }
---
+++ src/components/section/section_card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+import './section.css'
+
+function Section_Card() {
+	return <section className="section__card">
+                
+            </section>;
+}
+
+export default Section_Card;
